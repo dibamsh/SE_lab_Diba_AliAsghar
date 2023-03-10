8977d4f261e499fefa01ac575c90d8c2505420ef
AliLordLoss <a.a.ghanati@gmail.com>
2023-03-10 12:21:29 +0330

    - implemented subtract

--- calculator.py
+++ calculator.py
@@ -9,6 +9,14 @@ while True:
             print("answer:", op1 + op2)
         except:
             print("Invalid command!")
+    elif ('-' in phrase):
+        op1, op2 = phrase.split('-')
+        try:
+            op1 = int(op1)
+            op2 = int(op2)
+            print("answer:", op1 + op2)
+        except:
+            print("Invalid command!")
     else:
         print("Invalid command!")
     print()
