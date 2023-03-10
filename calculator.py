e04c1bc8b4aedd30ba230c2992516dc83549ccb6
AliLordLoss <a.a.ghanati@gmail.com>
2023-03-10 12:32:00 +0330

    - implemented divide & multipy functionalities

--- calculator.py
+++ calculator.py
@@ -19,6 +19,27 @@ while True:
         except:
             print("Invalid command!")
 
+    elif ('/' in phrase):
+        op1, op2 = phrase.split('/')
+        try:
+            op1 = float(op1)
+            op2 = float(op2)
+            if (op2 == 0):
+                print("Invalid Operand: can not divide by 0!")
+            else:
+                print("answer:", op1 / op2)
+        except:
+            print("Invalid command!")
+
+    elif ('*' in phrase):
+        op1, op2 = phrase.split('*')
+        try:
+            op1 = float(op1)
+            op2 = float(op2)
+            print("answer:", op1 * op2)
+        except:
+            print("Invalid command!")
+
     else:
         print("Invalid command!")
     print()
