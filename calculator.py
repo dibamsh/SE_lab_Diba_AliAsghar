6405f42bcb00adeaaf8f12f29fd9165a8a18cb1d
dibamsh <diba.masihi16@gmail.com>
2023-03-10 12:31:00 +0330

    code for mult & div.

--- calculator.py
+++ calculator.py
@@ -19,6 +19,29 @@ while True:
         except:
             print("Invalid command!")
 
+
+    elif ('*' in phrase):
+        op1, op2 = phrase.split('*')
+        try:
+            op1 = float(op1)
+            op2 = float(op2)
+            print("answer:", op1 * op2)
+        except:
+            print("Invalid command!")
+
+
+    elif ('/' in phrase):
+        op1, op2 = phrase.split('/')
+        try:
+            op1 = float(op1)
+            op2 = float(op2)
+            if op2 != 0:
+                print("answer:", op1 / op2)
+            else:
+                print("ERROR: divided by 0")
+        except:
+            print("Invalid command!")
+
     else:
         print("Invalid command!")
     print()
